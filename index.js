dd33f9507625cb9fd592eda055214fe573e6a984
xyz9999 <30603821+xyz9999@users.noreply.github.com>
2022-05-05 14:25:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World - 1\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+//importing node framework
+var express = require(‘express’);
+ 
+var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get(‘/’, function (req, res) {
+ res.send(‘hello world’);
+});
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
